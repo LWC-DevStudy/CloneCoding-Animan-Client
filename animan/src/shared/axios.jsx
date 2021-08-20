e14161905ff2f0778beb5567acd9efe3567eb032
Jongveloper <59111836+Jongveloper@users.noreply.github.com>
2021-08-20 19:56:56 +0900

    ✨ axios 서버 연결 전 작성

---
+++ animan/src/shared/axios.jsx
@@ -0,0 +1,21 @@
+// LIBRARY
+import axios from 'axios';
+
+// FUNCTION
+import { getToken } from './token';
+
+axios.defaults.withCredentials = true;
+
+const instance = axios.create({
+  baseURL: 'http://추후 입력/',
+});
+
+instance.interceptors.request.use((config) => {
+  config.headers['Content-Type'] = 'application/json; charset=utf-8';
+  config.headers['X-Requested-With'] = 'XMLHttpRequest';
+  config.headers['Accept'] = '*/*';
+  config.headers['authorization'] = getToken();
+  return config;
+});
+
+export default instance;
