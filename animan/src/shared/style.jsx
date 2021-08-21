c1707bdb0d6b0e9015e87ed3864cac3a31955555
sunm-in <sunm309@naver.com>
2021-08-22 03:00:52 +0900

    💄shared(style) - color 추가

--- animan/src/shared/style.jsx
+++ animan/src/shared/style.jsx
@@ -37,6 +37,7 @@ const theme = {
     brown: '91, 76, 61',
     buttonColor: '6, 88, 30',
     footerColor: '0, 72, 21',
+    green: '9, 120, 9',
     gray: '114, 114, 114',
     white: '255, 255, 255',
   },
