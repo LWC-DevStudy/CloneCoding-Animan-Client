ecf424945d169aa62e839036dbf5baca40e47ff3
sunm-in <sunm309@naver.com>
2021-08-22 00:48:35 +0900

    💄 components - Category

---
+++ animan/src/components/Comment.jsx
@@ -0,0 +1,22 @@
+// LIBRARY
+import React from 'react';
+import styled from 'styled-components';
+
+// ELEMENTS
+import { Grid } from '../elements/index';
+
+// STYLE
+
+// REDUX
+
+const Comment = () => {
+  return (
+    <Grid>
+      {/* 리뷰디테일에 들어가는 부분 */}
+      {/* 댓글 작성하는 Input */}
+      {/* 댓글 작성 버튼 */}
+    </Grid>
+  );
+};
+
+export default Comment;
