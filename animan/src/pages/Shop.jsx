13aefcd0b5174d99dd5692a10a80da00dc877d0e
sunm-in <sunm309@naver.com>
2021-08-22 00:50:04 +0900

    💄Shop(pages) 카테고리 추가

--- animan/src/pages/Shop.jsx
+++ animan/src/pages/Shop.jsx
@@ -1,12 +1,36 @@
 // LIBRARY
-import React from 'react'
+import React from 'react';
+import { css } from 'styled-components';
+
+// STYLE
+import { flexBox } from '../shared/style';
+
+// ELEMENTS
+import { Grid } from '../elements';
+
+// COMPONENTS
+import CategoryAll from '../components/CategoryAll';
+import CategoryPowder from '../components/CategoryPowder';
+import CategoryCookie from '../components/CategoryCookie';
 
 const Shop = () => {
   return (
-    <React.Fragment>
-      
-    </React.Fragment>
-  )
-}
+    <Grid
+      width="800px"
+      padding="20px"
+      margin="10vh 0 0 0"
+      al
+      addstyle={() => {
+        return css`
+          ${flexBox()}
+        `;
+      }}
+    >
+      <CategoryAll />
+      <CategoryPowder />
+      <CategoryCookie />
+    </Grid>
+  );
+};
 
 export default Shop;
