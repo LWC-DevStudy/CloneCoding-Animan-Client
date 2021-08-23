9a50a0f7b98526cb5e4a04c68201bb7e6caa04aa
Jongveloper <59111836+Jongveloper@users.noreply.github.com>
2021-08-23 15:18:58 +0900

    💄 리뷰 업데이트

--- animan/src/pages/Review.jsx
+++ animan/src/pages/Review.jsx
@@ -1,12 +1,54 @@
 // LIBRARY
-import React from 'react'
-
+import React from 'react';
+import ReviewCard from '../components/ReviewCard';
+// STYLE
+import styled, { css } from 'styled-components';
+// ELEMENTS
+import { Grid, Button } from '../elements/index';
+import { flexHoz } from '../shared/style';
 const Review = () => {
   return (
     <React.Fragment>
-      
+      <Grid margin="120px auto" width="1300px">
+        <Grid
+          margin="0 0 20px -8px"
+          addstyle={() => {
+            return css`
+              ${flexHoz('flex-end')}
+            `;
+          }}
+        >
+          <Button width="200px" bgColor="buttonColor" color="white">
+            글 작성
+          </Button>
+        </Grid>
+        <Grid
+          addstyle={() => {
+            return css`
+              display: flex;
+              flex-wrap: wrap;
+            `;
+          }}
+        >
+          <ReviewCard />
+          <ReviewCard />
+          <ReviewCard />
+          <ReviewCard />
+          <ReviewCard />
+          <ReviewCard />
+          <ReviewCard />
+          <ReviewCard />
+          <ReviewCard />
+          <ReviewCard />
+          <ReviewCard />
+          <ReviewCard />
+          <ReviewCard />
+          <ReviewCard />
+          <ReviewCard />
+        </Grid>
+      </Grid>
     </React.Fragment>
-  )
-}
+  );
+};
 
 export default Review;
