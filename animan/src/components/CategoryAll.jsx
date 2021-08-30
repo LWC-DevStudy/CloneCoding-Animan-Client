05788e1b0faa683fa55bec6a6ded86a535383d67
sunm-in <sunm309@naver.com>
2021-08-30 18:28:04 +0900

    🔨 카테고리 메뉴 이미지 적용

--- animan/src/components/CategoryAll.jsx
+++ animan/src/components/CategoryAll.jsx
@@ -3,24 +3,41 @@ import React from 'react';
 import styled from 'styled-components';
 
 // ROUTE
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 
 // ELEMENTS
 import { Grid } from '../elements/index';
 
 const CategoryAll = () => {
+  const path = useLocation().pathname;
   return (
     <Grid>
       <Link to="/shop">
         <CategoryAllImg
-          src={'https://cdn.imweb.me/thumbnail/20210720/b38ae9aead427.png'}
+          src={
+            path === '/shop'
+              ? 'https://cdn.imweb.me/thumbnail/20210720/35061dceacb3d.png'
+              : 'https://cdn.imweb.me/thumbnail/20210720/b38ae9aead427.png'
+          }
         />
       </Link>
       <Link to="/powder">
-        <CategoryPowderImg src="https://cdn.imweb.me/thumbnail/20210720/be9d6ef5b7cf8.png" />
+        <CategoryPowderImg
+          src={
+            path === '/powder'
+              ? 'https://cdn.imweb.me/thumbnail/20210720/98d89570a9ec5.png'
+              : 'https://cdn.imweb.me/thumbnail/20210720/be9d6ef5b7cf8.png'
+          }
+        />
       </Link>
       <Link to="cookie">
-        <CategoryCookieImg src="https://cdn.imweb.me/thumbnail/20210720/a629b0e02f2ba.png" />
+        <CategoryCookieImg
+          src={
+            path === '/cookie'
+              ? 'https://cdn.imweb.me/thumbnail/20210720/866042c27ca1b.png'
+              : 'https://cdn.imweb.me/thumbnail/20210720/a629b0e02f2ba.png'
+          }
+        />
       </Link>
     </Grid>
   );
