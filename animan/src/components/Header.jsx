236444b231b95e2553e92c07d105abba1ca0c50e
Jongveloper <59111836+Jongveloper@users.noreply.github.com>
2021-09-02 16:33:15 +0900

    ✨헤더 로그인 체크 구현

--- animan/src/components/Header.jsx
+++ animan/src/components/Header.jsx
@@ -1,5 +1,7 @@
 // LIBRARY
 import React from 'react';
+import { useDispatch } from 'react-redux';
+import { useSelector } from 'react-redux';
 
 // STYLE
 import styled, { css } from 'styled-components';
@@ -14,7 +16,95 @@ import { Grid, Text } from '../elements/index';
 // IMAGE
 import Logo from '../shared/images/MainImage.png';
 
+// REDUX
+import { removeToken } from '../shared/token';
+import { logInCheck } from '../redux/modules/user';
+
 function Header() {
+  const dispatch = useDispatch();
+  const logOut = () => {
+    removeToken();
+    window.location.reload();
+  };
+  const is_login = useSelector((state) => state.user.is_login);
+
+  React.useEffect(() => {
+    dispatch(logInCheck());
+  });
+  if (is_login) {
+    return (
+      <HeaderStyle>
+        <Grid
+          width="100%"
+          padding="10px"
+          addstyle={() => {
+            return css`
+              ${flexBox('inline-flex')}
+            `;
+          }}
+        >
+          <Grid
+            margin="0 0 0 -2%"
+            width="100%"
+            addstyle={() => {
+              return css`
+                ${flexBox('flex-start')}
+              `;
+            }}
+          >
+            <Link style={{ textDecoration: 'none' }} to="/login">
+              <Text
+                margin="0px 0px 0px 60px"
+                width="100px"
+                color="gray"
+                size="16px"
+                clickEvent={logOut}
+              >
+                LOGOUT
+              </Text>
+            </Link>
+            <Link style={{ textDecoration: 'none' }} to="/cart">
+              <Text color="gray" size="16px">
+                CART
+              </Text>
+            </Link>
+          </Grid>
+          <Grid
+            margin="0 0 0 4%"
+            addstyle={() => {
+              return css`
+                ${flexHoz('center')}
+              `;
+            }}
+          >
+            <Link to="/">
+              <img src={Logo} alt="로고" width="130px" />
+            </Link>
+          </Grid>
+          <Grid
+            margin="0 2% 0 0"
+            width="100%"
+            addstyle={() => {
+              return css`
+                ${flexHoz('flex-end')}
+              `;
+            }}
+          >
+            <Link style={{ textDecoration: 'none' }} to="/shop">
+              <Text color="gray" width="90px" size="19px">
+                SHOP
+              </Text>
+            </Link>
+            <Link style={{ textDecoration: 'none' }} to="/review">
+              <Text color="gray" size="19px">
+                REVIEW
+              </Text>
+            </Link>
+          </Grid>
+        </Grid>
+      </HeaderStyle>
+    );
+  }
   return (
     <HeaderStyle>
       <Grid
@@ -74,12 +164,12 @@ function Header() {
         >
           <Link style={{ textDecoration: 'none' }} to="/shop">
             <Text color="gray" width="90px" size="19px">
-              Shop
+              SHOP
             </Text>
           </Link>
           <Link style={{ textDecoration: 'none' }} to="/review">
             <Text color="gray" size="19px">
-              Review
+              REVIEW
             </Text>
           </Link>
         </Grid>
